f3a821457ffb3e502948462998d8bae3f1f30125
thomaspaulwhalan <78250325+thomaspaulwhalan@users.noreply.github.com>
2021-10-22 18:39:03 +1100

    Create pstring.h

---
+++ pstring.h
@@ -0,0 +1,24 @@
+#ifndef pstring_h
+#define pstring_h
+
+#ifndef stdio_h
+#include<stdio.h>
+#endif
+#ifndef stdbool_h
+#include<stdbool.h>
+#endif
+
+#ifndef pchar_h
+#include<pchar.h>
+#endif
+
+bool		pstartswith	(char *string, char character);
+bool		pendswith	(char *string, char character);
+int		pcount		(const char *string, char character);
+char		*plower		(char *string);
+char		*pupper		(char *string);
+char		*pcapitalize	(char *string);
+char		*pswapcase	(char *string);
+char		*ptitle		(char *string);
+
+#endif // pstring_h
